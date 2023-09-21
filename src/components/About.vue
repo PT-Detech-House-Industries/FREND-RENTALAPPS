dc8e9d676a7a171cb112edcb46d22acb7e4373f8
Edward Kristian Mangare, S.Kom <37824502+krizedward@users.noreply.github.com>
2023-09-21 11:07:20 +0700

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Tentang Kami</h1>
+    <p>Kami adalah perusahaan yang luar biasa.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage'
+}
+</script>
+
+<style scoped>
+/* Gaya khusus untuk komponen About */
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <h1>Selamat datang di halaman Beranda!</h1>
+    <p>Ini adalah halaman Beranda aplikasi kami.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<style scoped lang="scss">
+/* Gaya khusus untuk komponen Home */
+.home {
+  h1 {
+    color: aqua;
+  }
+
+  p {
+    color: red;
+  }
+}
+</style>
