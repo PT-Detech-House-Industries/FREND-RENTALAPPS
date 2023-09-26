f6513bf3d9011ab2652cd67665b32fb2b9b0f429
Edward Kristian Mangare, S.Kom <37824502+krizedward@users.noreply.github.com>
2023-09-26 10:13:23 +0700

    update 1.0.6
    FREND-RENTALAPPS :
    - Version 1.0.6
    melakukan pembaruan :
    - membuat header masih bug
    - menambahkan jquery untuk membantu effect ui
    
    belum :
    - design fix untuk dibangun.
    - baner untuk nihon uwu.
    - kurang merapikan halaman home,about,contact.

--- src/components/dokumentasi/TopMenuV1.vue
+++ src/components/dokumentasi/TopMenuV1.vue
@@ -3,62 +3,23 @@
     <router-link to="/">
       <img src="../assets/png/logo.png" alt="Logo" class="logo">
     </router-link>
-    <button class="kanan" @click="toggleElement">Toggle Element</button>
-    <div id="myElement" class="merah" :class="{ 'active': isActive }">
-      <div class="menu-option">
-        <router-link to="/dashboard">Beranda</router-link>
-      </div>
-      <div class="menu-option">
-        <router-link to="/about">Tentang</router-link>
-      </div>
-      <div class="menu-option">
-        <router-link to="/contact">Kontak</router-link>
-      </div>
+    <div class="menu-option">
+      <router-link to="/dashboard">Beranda</router-link>
+    </div>
+    <div class="menu-option">
+      <router-link to="/about">Tentang</router-link>
+    </div>
+    <div class="menu-option">
+      <router-link to="/contact">Kontak</router-link>
     </div>
   </div>
 </template>
 
 <script>
-import $ from 'jquery';
-
-export default {
-  data() {
-    return {
-      isActive: false,
-    };
-  },
-  methods: {
-    // add function
-    toggleElement() {
-      this.isActive = !this.isActive; //true
-    },
-  },
-  mounted() {
-    // Memanipulasi elemen dengan jQuery saat komponen di-mount
-    $('#myElement').addClass('active');
-
-    $(document).ready(function() {
-      $("p").click(function() {
-        $(this).fadeOut();
-      });
-    }); // untuk memanipulasi elemen p, menghilang
-  },
-  beforeDestroy() {
-    // Membersihkan event-handler atau elemen yang dimanipulasi dengan jQuery
-    $('#myElement').off(); // Hapus event-handler jika ada
-  },
-};
 </script>
 
 <style scoped lang="scss">
   @import '@/scss/_theme.scss';
-
-  .logo {
-    width: 50px;
-    /* Sesuaikan ukuran logo */
-    height: auto;
-    float: left;
-  }
 
   .top-menu {
     background-color: $primary-color;
@@ -88,9 +49,9 @@ export default {
 
   /* Gaya untuk pilihan menu */
   .menu-option {
-    font-size: 12px;
+    font-size: 24px;
     /* Ukuran font 24px */
-    padding: 2px 4px;
+    padding: 10px 20px;
     /* Sesuaikan dengan gaya Anda */
     cursor: pointer;
     /* Menambahkan tanda kursor saat dihover */
@@ -111,17 +72,12 @@ export default {
     color: inherit;
     /* Warisan warna teks dari parent (.menu-option) */
   }
-</style> <!-- untuk header  -->
+</style>
 
-<style>
-.kanan {
-  float: right;
-}
-
-.active {
-  background-color: red;
-  padding: 10px;
-  width: auto;
-  transition: background-color 0.3s ease, padding 0.3s ease;
-}
-</style><!-- untuk active -->
+<style scoped lang="scss">
+  .logo {
+    width: 50px;
+    /* Sesuaikan ukuran logo */
+    height: auto;
+  }
+</style><!-- logo -->
